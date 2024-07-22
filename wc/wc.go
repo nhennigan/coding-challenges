02d76bcc33cb9634b7fa5cb3ae60d5a6a9c1aa7e
nhennigan <niamhhennigan@gmail.com>
2024-07-22 16:28:40 -0700

    step 2

--- wc/wc.go
+++ wc/wc.go
@@ -1,29 +1,53 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
 
 func count(fileName string) {
-	//file, err := os.Open(fileName)
 	fileInfo, err := os.Stat(fileName)
 	if err != nil {
 		fmt.Println(err)
 	}
-	//defer file.Close()
 	fileSize := fileInfo.Size()
 	fmt.Printf("The file is %d bytes long\n", fileSize)
 }
 
+func lines(fileName string) {
+	lineCount := 0
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lineCount++
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("The file is %d lines long\n", lineCount)
+}
+
 func main() {
 
+	//need to add check to make sure 2 args set
 	action := os.Args[1]
+	fileName := os.Args[2]
 
 	switch action {
 	case "-c":
 		fileName := os.Args[2]
 		count(fileName)
+	case "-l":
+		lines(fileName)
+	// case "-w":
+	// 	words(fileName)
 	default:
 		fmt.Println("No function matching", action)
 	}
