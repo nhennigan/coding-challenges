de5112cc44bf4a106ee5318674041dc34f7d4afd
nhennigan <niamhhennigan@gmail.com>
2024-07-22 17:11:53 -0700

    step three

--- wc/wc.go
+++ wc/wc.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func count(fileName string) {
@@ -34,6 +35,38 @@ func lines(fileName string) {
 	fmt.Printf("The file is %d lines long\n", lineCount)
 }
 
+func words(fileName string) {
+	wordCount := 0
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		words := strings.Split(scanner.Text(), " ")
+		for _, w := range words {
+			if w != "" {
+				wordCount++
+			}
+		}
+		//fmt.Println(words)
+		// if len(words) == 1 && words[0] == "" {
+		// 	continue
+		// } else if words[0] == "\n" {
+		// 	continue
+		// }
+		// wordCount += len(words)
+		fmt.Println(wordCount)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("The file is %d words long\n", wordCount)
+}
+
 func main() {
 
 	//need to add check to make sure 2 args set
@@ -46,8 +79,8 @@ func main() {
 		count(fileName)
 	case "-l":
 		lines(fileName)
-	// case "-w":
-	// 	words(fileName)
+	case "-w":
+		words(fileName)
 	default:
 		fmt.Println("No function matching", action)
 	}
